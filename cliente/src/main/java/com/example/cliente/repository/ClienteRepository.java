5647b79fd768379c461f0da121433fdb3b39000b
jherson91fs <jherson.fernandez.19@gmail.com>
2024-04-01 10:29:48 -0500

    dsagds

--- cliente/src/main/java/com/example/cliente/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cliente.repository;
-
-import com.example.cliente.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
--- ms-cliente/src/test/java/com/example/mscliente/MsClienteApplicationTests.java
+++ ms-cliente/src/test/java/com/example/mscliente/MsClienteApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.cliente;
+package com.example.mscliente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClienteApplicationTests {
+class MsClienteApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
